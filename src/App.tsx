1696b6f4b61783f576f1fb72861d006358867f8b
theSwordBreaker <kunalguptadev007@gmail.com>
2022-10-05 05:12:31 +0530

    changed the default step

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import React, { useEffect, useState } from "react"
 import Steps from "./component/Steps"
 
 function App() {
-  const [count, setCount] = useState(3)
+  const [count, setCount] = useState(0)
   const [active, setActive] = useState(true)
   const next = () => setCount((c) => (c < 4 ? ++c : 4))
 
